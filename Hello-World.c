0b365e804be7b810695542c0584a7e5e83464c5b
whoisyashu <130261169+whoisyashu@users.noreply.github.com>
2023-12-06 06:36:59 +0530

    Update Hello-World.c

--- Hello-World.c
+++ Hello-World.c
@@ -1,5 +1,5 @@
-#include<stdio.h>//Basic Header file
-int main() //main () is my funtion
-{ //compiler start execution from here
-  printf("Hello World!");//printf-> to print something
+#include<stdio.h>
+int main()
+{
+  printf("Hello World!");
 }
